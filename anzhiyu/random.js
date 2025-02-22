245776ac58b6f9d7822e8e14a0fa7bbe295adcac
xiaohj233 <txj090119@163.com>
2025-02-22 21:18:40 +0800

    Site updated: 2025-02-22 21:18:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/15/哦耶~/"];function toRandomPost(){
+var posts=["2025/02/15/哦耶~/","2025/02/22/网站上线一周/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
