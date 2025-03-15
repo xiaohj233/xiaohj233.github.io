596f6b6e44bfc91c11ff955f9548d5b9cfe55ac8
xiaohj233 <txj090119@163.com>
2025-03-15 22:53:39 +0800

    Site updated: 2025-03-15 22:53:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/15/哦耶~/","2025/02/22/网站上线一周/","2025/02/23/先帝更新未半，而中道放弃/"];function toRandomPost(){
+var posts=["2025/02/15/哦耶~/","2025/02/22/网站上线一周/","2025/02/23/先帝更新未半，而中道放弃/","2025/03/15/朋友的意义/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
