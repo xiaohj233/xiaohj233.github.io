a8f722114bee0f02f728a0297bf9aa372ab4e4b8
xiaohj233 <txj090119@163.com>
2025-09-05 01:27:23 +0800

    Site updated: 2025-09-05 01:27:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/32fe1627/","posts/2395baf2/","posts/c7a123cf/","posts/5fbd7f47/","posts/16f800fe/","posts/1e3d76ff/","posts/f94a695e/","posts/20b4b5b9/","posts/da0134c2/","posts/806dfce3/","posts/e0c9a0da/"];function toRandomPost(){
+var posts=["posts/32fe1627/","posts/2395baf2/","posts/c7a123cf/","posts/1e3d76ff/","posts/16f800fe/","posts/806dfce3/","posts/5fbd7f47/","posts/f94a695e/","posts/20b4b5b9/","posts/da0134c2/","posts/e0c9a0da/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
